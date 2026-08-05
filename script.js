80864d1fba25766939bc1db2684ebd0b91c44e19
bmw72033-droid <bmw72033@gmail.com>
2026-08-05 21:49:50 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://ezirapbynmyndvtwrefj.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_vROJEoA2pQ-9OHMUUCgLvA_8X_Ss_Rb";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
